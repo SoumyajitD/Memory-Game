e4d20bfc075825400afb339b567f0d7a69482b30
Soumyajit Dawn <soumyajitdawn@gmail.com>
2021-08-21 12:47:25 +0530

    css props

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var start=document.getElementById("start");
 var board=document.getElementById("board");
 
-var arr=["1","2","3","1","2","3"];
+var arr=["1","2","3","4","1","2","3","4"];
 
 start.addEventListener('click',startGame);
 
@@ -9,10 +9,13 @@ function startGame(){
     var html="";
     var k=1;
     for(var image of arr){
-        html+="<img src='assets/blank.jpg' onClick=check('"+image+"','"+k+"') id='"+k+"'/>";
+        html+="<img src='assets/blank.jpg' style='max-width:15%' onClick=check('"+image+"','"+k+"') id='"+k+"'/>";
+        if(k==4){
+            html+="<<br>";
+        }
         k++;
     }
-    alert(k);
+    // alert(k);
     board.innerHTML=html;
 }
     function  check(image,k){
