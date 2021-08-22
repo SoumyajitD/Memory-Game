f26de2b20e6ba647b095eaa963ed7750cdcf4ee2
Soumyajit Dawn <soumyajitdawn@gmail.com>
2021-08-22 23:31:03 +0530

    basic functionalities

--- game.js
+++ game.js
@@ -1,47 +1,63 @@
-var start=document.getElementById("start");
-var board=document.getElementById("board");
-var first=0;
-firstID=0;
+var start = document.getElementById("start");
+var board = document.getElementById("board");
+var first = 0;
+firstID = 0;
 
-var arr=["1","2","3","4","1","2","3","4"];
-var cArr=["0","0"];
+var arr = ["1", "2", "3", "4", "1", "2", "3", "4"];
+var cArr = ["0", "0"];
+var arrClicked=[];
 
-start.addEventListener('click',startGame);
+start.addEventListener('click', startGame);
 
-function startGame(){
-    var html="";
-    var k=1;
-    for(var image of arr){
-        html+="<img src='assets/blank.jpg' style='max-width:15%' onClick=check('"+image+"','"+k+"') id='"+k+"'/>";
-        if(k==4){
-            html+="<br>";
+function startGame() {
+    var html = "";
+    var k = 1;
+    for (var image of arr) {
+        html += "<img src='assets/blank.jpg' style='max-width:15%' onClick=check('" + image + "','" + k + "') id='" + k + "'/>";
+        if (k == 4) {
+            html += "<br>";
         }
         k++;
     }
     // alert(k);
-    board.innerHTML=html;
+    board.innerHTML = html;
 }
-    function  check(image,k){
-        if(first==0){
-            first=image;
-            firstID=k;
+function check(image, k) {
+    if (firstID != k) {
+        if (first == 0) {
+
+            first = image;
+            firstID = k;
+            document.getElementById(k).src = "assets/" + image + ".jpg";
         }
-        else{
-            if(first==image){
-                first=0;
+        else {
+            if (first == image) {
+
+                
+                document.getElementById(k).src = "assets/" + image + ".jpg";
                 // alert("correct");
-                document.getElementById(k).src="assets/"+image+".jpg";
-                document.getElementById(firstID).src="assets/"+image+".jpg";
-                firstID=0;
+
+
+
             }
-            else{
+            else {
                 // alert("wrong");
-              
-                first=0;
+                // console.log(firstID);
+                // console.log(k);
+                document.getElementById(k).src = "assets/" + image + ".jpg";
+                setTimeout(function () {
 
+                    document.getElementById(firstID).src = "assets/" + 'blank' + ".jpg";
+                    document.getElementById(k).src = "assets/" + 'blank' + ".jpg";
+                    firstID = 0;
+                    first = 0;
+                }, 500);
             }
-
+            first = 0;
         }
-        document.getElementById(k).src="assets/"+image+".jpg";
-
     }
+
+
+
+
+}
